74261deaff092abdc8170b1655f93e3c702b3e71
Natasha <natasha.kelkar@gmail.com>
2015-12-14 10:57:01 +0200

    changed from pysvn to svn command line because of incompatibility with 64 bit Python for Maya and Nuke

--- utils/subversion_utils.py
+++ utils/subversion_utils.py
@@ -1,13 +1,12 @@
 __author__ = 'Natasha'
 
-import pysvn
 import subprocess
 import os
 import logging
 
 tortoiseSVN = 'C:\\Program Files\\TortoiseSVN\\bin\\TortoiseProc.exe'
+svn = 'C:\\Program Files\\Apache-Subversion-1.9.2\\bin\\svn.exe'
 outfile = os.path.join(os.environ['TEMP'], 'repo.txt')
-client = pysvn.Client()
 
 
 def openRepoBrowser():
@@ -23,51 +22,73 @@ def getSelectedRepo():
 
 
 def checkout(url, path):
+    cmd = '"%s" checkout "%s" "%s"' % (svn, url, path)
     try:
-        client.checkout(url, path)
+        subprocess.Popen(cmd)
     except ValueError:
         logging.error('SVN Checkout error: url=%s path=%s' % (url, path))
 
 
 def add(path):
+    cmd = '"%s" add "%s"' % (svn, path)
     try:
-        client.add(path)
+        subprocess.Popen(cmd)
     except ValueError:
         logging.error('SVN Add error: path=%s' % path)
 
 
 def commit(path, message):
+    cmd = '"%s" commit -m "%s" "%s"' % (svn, message, path)
     try:
-        client.checkin(path, message)
+        subprocess.Popen(cmd)
     except ValueError:
         logging.error('SVN checkin error: path=%s' % path)
 
 
 def status(path):
-    stats = 'Error'
-    try:
-        stats = client.status(path, recurse=False)[0]
-        textStatus = str(stats['text_status'])
-        if textStatus == 'normal':
+    textStatus = ''
+    if os.path.isdir(path):
+        cmd = '"%s" status --depth empty -v "%s"' % (svn, path)
+    else:
+        cmd = '"%s" status -v "%s"' % (svn, path)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    process.kill()
+    fields = out.split()
+    if fields:
+        if not fields[0].isdigit():
+            if fields[0] == 'M':
+                textStatus = 'modified'
+            elif fields[0] == 'A':
+                textStatus = 'added'
+            elif fields[0] == '?':
+                textStatus = 'unversioned'
+        else:
             textStatus = 'versioned'
-    except:
-        textStatus = "unversioned"
+    if err:
+        textStatus = 'unversioned'
     return textStatus
 
 
 def update(path):
+    cmd = '"%s" update "%s"' % (svn, path)
     try:
-        client.update(path)
+        subprocess.Popen(cmd)
     except ValueError:
         logging.error('SVN update error: path=%s' % path)
 
 
 def getVersionNumber(path):
     revNo = None
-    try:
-        info = client.info(path)
-        rev = info['commit_revision']
-        revNo = rev.number
-    except ValueError:
-        logging.error('SVN version error: path=%s' % path)
+    cmd = '"%s" status -v "%s"' % (svn, path)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    process.kill()
+    fields = out.split()
+    for f in fields:
+        if f.isdigit():
+            revNo = f
+            break
     return revNo
